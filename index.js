c684f47b58864f801fe47405eb31044b657ae3ba
rahulinfolabz <rahulinfolabz@gmail.com>
2026-03-26 11:29:30 +0530

    cors updated

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "http://localhost:5174"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
